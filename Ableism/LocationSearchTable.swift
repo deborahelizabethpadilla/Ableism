462f7d00fa45db29c955b96eb2eef0832e062c8d
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-26 02:29:06 -0500

    Storyboard

---
+++ Ableism/LocationSearchTable.swift
@@ -0,0 +1,14 @@
+//
+//  LocationSearchTable.swift
+//  Ableism
+//
+//  Created by Deborah on 2/26/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchTable: UITableViewController {
+    
+    
+}
