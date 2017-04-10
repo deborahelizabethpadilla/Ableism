d78e480ef5d19f408cbae3156e6ce1496d69b4ad
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-04-10 17:50:06 -0400

    Edit: Redo Storyboard
    Design changes.

---
+++ Ableism/TabBarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarViewController.swift
+//  Ableism
+//
+//  Created by Deborah on 4/10/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
