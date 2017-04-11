6accd8d49571b21fc84ebd71636d543ee15bf8c7
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-04-11 12:50:48 -0400

    Edit Pods

--- Pods/Target Support Files/Pods-Ableism/Pods-Ableism-frameworks.sh
+++ Pods/Target Support Files/Pods-Ableism/Pods-Ableism-frameworks.sh
@@ -87,15 +87,6 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/TextFieldEffects/TextFieldEffects.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/TextFieldEffects/TextFieldEffects.framework"
-fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
