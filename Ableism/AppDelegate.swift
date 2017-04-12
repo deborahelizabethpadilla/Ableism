cbfd989fe7d8ce2fd09a3dd7a65b485c59a49e80
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-04-11 22:02:50 -0400

    Edit: Redo Storyboard

--- Ableism/AppDelegate.swift
+++ Ableism/AppDelegate.swift
@@ -16,7 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -35,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+       
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- Ableism/TabBarViewController.swift
+++ Ableism/TabBarViewController.swift
@@ -2,23 +2,26 @@
 //  TabBarViewController.swift
 //  Ableism
 //
-//  Created by Deborah on 4/10/17.
+//  Created by Deborah on 4/11/17.
 //  Copyright © 2017 Deborah. All rights reserved.
 //
 
 import UIKit
+import ChameleonFramework
 
 class TabBarViewController: UITabBarController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        //Set Custom Background
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Custom Tab Bar Background
+        
+        self.tabBar.barTintColor = FlatGreen()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
